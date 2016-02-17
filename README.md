6a9c20e9a7875480b75ebeefbce7930c13dc2704
Erik Carlsson <info@code301.com>
2016-02-17 12:43:27 +0000

    Dev

---
+++ README.md
@@ -0,0 +1,38 @@
+# NAME
+
+Badge::Depot::Plugin::Kwalitee - Short intro
+
+![Requires Perl 5.10+](https://img.shields.io/badge/perl-5.10+-brightgreen.svg) [![Travis status](https://api.travis-ci.org/Csson/p5-Badge-Depot-Plugin-Kwalitee.svg?branch=master)](https://travis-ci.org/Csson/p5-Badge-Depot-Plugin-Kwalitee) 
+
+# VERSION
+
+Version 0.0100, released 2016-02-17.
+
+# SYNOPSIS
+
+    use Badge::Depot::Plugin::Kwalitee;
+
+# DESCRIPTION
+
+Badge::Depot::Plugin::Kwalitee is ...
+
+# SEE ALSO
+
+# SOURCE
+
+[https://github.com/Csson/p5-Badge-Depot-Plugin-Kwalitee](https://github.com/Csson/p5-Badge-Depot-Plugin-Kwalitee)
+
+# HOMEPAGE
+
+[https://metacpan.org/release/Badge-Depot-Plugin-Kwalitee](https://metacpan.org/release/Badge-Depot-Plugin-Kwalitee)
+
+# AUTHOR
+
+Erik Carlsson <info@code301.com>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Erik Carlsson.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Badge/Depot/Plugin/Kwalitee.pm
+++ lib/Badge/Depot/Plugin/Kwalitee.pm
@@ -8,7 +8,91 @@ package Badge::Depot::Plugin::Kwalitee;
 # AUTHORITY
 our $VERSION = '0.0100';
 
+use Moose;
+use namespace::autoclean;
+use MooseX::AttributeShortcuts;
+use Types::Standard qw/Str HashRef/;
+use Types::URI qw/Uri/;
+use JSON::MaybeXS 'decode_json';
+use Path::Tiny;
+with 'Badge::Depot';
 
+has dist => (
+    is => 'ro',
+    isa => Str,
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        if($self->has_meta) {
+            return $self->_meta->{'dist'} if exists $self->_meta->{'dist'};
+        }
+    },
+);
+has version => (
+    is => 'ro',
+    isa => Str,
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        if($self->has_meta) {
+            return $self->_meta->{'version'} if exists $self->_meta->{'version'};
+        }
+    },
+);
+has base_url => (
+    is => 'ro',
+    isa => Uri,
+    coerce => 1,
+    lazy => 1,
+    default => 'https://badgedepot.code301.com',
+);
+has custom_image_url => (
+    is => 'ro',
+    isa => Uri,
+    coerce => 1,
+    lazy => 1,
+    builder => 1,
+);
+
+sub _build_custom_image_url  {
+    my $self = shift;
+    return sprintf '%s/badge/kwalitee/%s/%s', $self->base_url, $self->dist, $self->version;
+}
+has _meta => (
+    is => 'ro',
+    isa => HashRef,
+    predicate => 'has_meta',
+    builder => '_build_meta',
+);
+
+sub _build_meta {
+    my $self = shift;
+
+    return {} if !path('META.json')->exists;
+
+    my $json = path('META.json')->slurp_utf8;
+    my $data = decode_json($json);
+
+    return {} if !exists $data->{'name'} || !exists $data->{'version'};
+
+    return {
+        dist => $data->{'name'},
+        version => $data->{'version'},
+    };
+}
+
+sub BUILD {
+    my $self = shift;
+    $self->link_url(sprintf 'http://cpants.cpanauthors.org/dist/%s%s', $self->dist, $self->version eq 'latest' ? '' : '-'.$self->version);
+    $self->image_url($self->custom_image_url);
+    $self->image_alt('Distribution kwalitee');
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
 
 1;
 
