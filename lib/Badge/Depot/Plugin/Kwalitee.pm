9d6635e96d96c90ad4172b7d0926b8c421bcd21e
Erik Carlsson <info@code301.com>
2016-02-22 14:40:29 +0000

    Bump version after 0.0101

--- lib/Badge/Depot/Plugin/Kwalitee.pm
+++ lib/Badge/Depot/Plugin/Kwalitee.pm
@@ -6,7 +6,7 @@ package Badge::Depot::Plugin::Kwalitee;
 
 # ABSTRACT: Kwalitee badge for Badge::Depot
 # AUTHORITY
-our $VERSION = '0.0100';
+our $VERSION = '0.0102';
 
 use Moose;
 use namespace::autoclean;
